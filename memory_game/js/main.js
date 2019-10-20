fe5c9bb0059cd8dc2c6bffa3a970d019319d5efc
Dide Mertens <d.g.j.mertens@gmail.com>
2019-10-20 16:35:50 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
 
+const cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry. Try again!");
+  }
+}
