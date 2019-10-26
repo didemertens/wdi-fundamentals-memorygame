1813e11f1f091b2a493d32e7c52a1a42171eaae1
Dide Mertens <d.g.j.mertens@gmail.com>
2019-10-26 16:12:04 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,41 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 }
 
 function flipCard(cardID) {
-  console.log("User flipped " + cards[cardID]);
-  cardsInPlay.push(cards[cardID]);
+  console.log("User flipped " + cards[cardID].rank);
+  cardsInPlay.push(cards[cardID].rank);
+  console.log(cards[cardID].cardImage);
+  console.log(cards[cardID].suit);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
